dd70068acaa1eb04fab548ce30250afc340e10a2
Matti Tahvonen <matti@vaadin.com>
2018-06-06 15:30:05 +0300

    wip

--- src/main/java/com/vaadin/starter/skeleton/spring/ExampleServletInitializer.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/ExampleServletInitializer.java
@@ -2,23 +2,15 @@ package com.vaadin.starter.skeleton.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
-public class ExampleServletInitializer extends SpringBootServletInitializer {
+public class ExampleServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleServletInitializer.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(
-            SpringApplicationBuilder builder) {
-        return builder.sources(ExampleServletInitializer.class);
     }
 
 }
